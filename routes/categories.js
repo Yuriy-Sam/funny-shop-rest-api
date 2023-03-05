f32058a02877f32c4834bd1bff70c6aa67c57f30
Yuriy Samoilenko <yuriy.sam2002@gmail.com>
2023-03-05 13:31:05 +0100

    update data

--- routes/categories.js
+++ routes/categories.js
@@ -1,35 +1,14 @@
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+const { categories } = require("../data.json");
 
+let router = express.Router();
 
-const CART_ITEMS = [
-    {
-    "name": "gadgets",
-    "value": "Gadgets"
-    },
-    {
-    "name": "computing",
-    "value": "Computing"
-    },
-    {
-    "name": "photo",
-    "value": "Photo"
-    },
-    {
-    "name": "tourism",
-    "value": "Tourism"
-    },
-    {
-    "name": "tv",
-    "value": "TV"
-    }
-]
+const CART_ITEMS = categories;
 
-router.use(express.json())
+router.use(express.json());
 
-router.get("/", function(req, res) {
-    res.status(200).send(CART_ITEMS);
+router.get("/", function (req, res) {
+  res.status(200).send(CART_ITEMS);
 });
-
 
 module.exports = router;
